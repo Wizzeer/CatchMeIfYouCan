23267a519858226fe11252db076b8b5dd4a2ed23
Dominik Sowa <wizzeer@gmail.com>
2019-02-03 22:29:59 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,140 @@
+let uluru, map, marker
+let ws
+let players = {}
+let nick = '1'
+let sendButton = document.querySelector('button');
+let msg;
+let flag=false;
+
+function search(e) {
+    if(event.key == 'Enter') {
+        sendMessage();      
+    }
+}
+
+function initMap() {
+    uluru = { lat: -25.363, lng: 131.044 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: uluru,
+        keyboardShortcuts: false
+    });
+    
+    marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: 'owl2.png'
+    });
+    getLocalization()
+    startWebSocket()
+    addKeyboardEvents()
+    console.log("Siema");
+}
+
+function addKeyboardEvents() {
+    window.addEventListener('keydown', poruszMarkerem)
+}
+function poruszMarkerem(ev) {
+    let lat = marker.getPosition().lat()
+    let lng = marker.getPosition().lng()
+
+    switch (ev.code) {
+        case 'ArrowUp':
+            lat += 0.1
+            break;
+        case 'ArrowDown':
+            lat -= 0.1
+            break;
+        case 'ArrowLeft':
+            lng -= 0.1
+            break;
+        case 'ArrowRight':
+            lng += 0.1
+            break;
+    }
+    let position = {
+        lat,
+        lng
+    }
+    let wsData = {
+        lat: lat,
+        lng: lng,
+        id: nick
+    }
+    marker.setPosition(position)
+    ws.send(JSON.stringify(wsData))
+}
+function startWebSocket() {
+    let url = 'ws://91.121.6.192:8010'
+    ws = new WebSocket(url)
+    ws.addEventListener('open', onWSOpen)
+    ws.addEventListener('message', onWSMessage)
+    
+    
+}
+
+function onWSOpen(data) {
+    console.log(data)
+}
+
+function sendMessage(){
+    let text = document.getElementById('text');
+    let nickname = document.getElementById('nick').value;
+    textToSend=nickname+": "+text.value;
+    msg = { typ: 'msg', tekst: textToSend };
+    ws.send(JSON.stringify(msg));
+    text.value="";
+}
+
+function onWSMessage(e) {
+    let log = document.getElementById('log');
+        
+
+        
+    msg=(JSON.parse(e.data));
+    if(msg.typ=='msg'){
+        log.innerHTML+=(msg.tekst)+"<br/>";
+    }
+
+    else{
+        let data = JSON.parse(e.data)
+
+        if (!players['user' + data.id]) {
+            players['user' + data.id] = new google.maps.Marker({
+                position: { lat: data.lat, lng: data.lng },
+                map: map,
+                animation: google.maps.Animation.DROP
+            })
+        } else {
+            players['user' + data.id].setPosition({
+                lat: data.lat,
+                lng: data.lng
+            })
+        }
+    }
+        
+    
+
+    
+}
+
+
+
+function getLocalization() {
+    navigator.geolocation.getCurrentPosition(geoOk, geoFail)
+
+}
+
+function geoOk(data) {
+    let coords = {
+        lat: data.coords.latitude,
+        lng: data.coords.longitude
+    }
+    map.setCenter(coords)
+    marker.setPosition(coords)
+}
+
+function geoFail(err) {
+    console.log(err)
+}
